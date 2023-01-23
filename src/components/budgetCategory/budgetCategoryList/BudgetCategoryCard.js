9807f65c960184991f3d62b84bed523571bc810c
InnocentOmenka <innocent.omenka@decagon.dev>
2023-01-23 23:41:53 +0100

    aligned budget category list

--- src/components/budgetCategory/budgetCategoryList/BudgetCategoryCard.js
+++ src/components/budgetCategory/budgetCategoryList/BudgetCategoryCard.js
@@ -3,14 +3,11 @@ import "./BudgetCategoryList.css";
 
 const BudgetCategoryCard = ({ item }) => {
   return (
-    <div>
-      <div className="frame-8722-XPf">
+      <div className="frame-8722-XPf budgetCategoryList">
         <div className="frame-8717-FaZ">
-          <img className="transport-ory" src="/assets/transport-wjX.png" />
           <p className="transportation-YJm">{item.name}</p>
         </div>
         <img className="option-sM3" src="/assets/option-uxu.png" />
-      </div>
     </div>
   );
 };
